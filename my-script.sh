8ce2c92b7ccba4642576a8724b09646689043384
Smail Kalić <68398301+kalicx02@users.noreply.github.com>
2022-10-19 23:56:30 +0200

    my-script.sh add
    15.10.2022. - BS Predavanje

---
+++ my-script.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+FILE=""
+
+function help {
+  echo "Usage:  Programm $0 [-h] -f file"
+  echo "-h:  Help (this text)"
+  echo "-f:  File to be searched"
+  exit
+}
+
+while test $# -ne 0; do
+  case "$1" in
+  -h) help
+    break;;
+  -f) FILE = $2; shift;;
+  *) help
+    break;;
+  esac
+  shift
+done
+
+if test -z "$FILE" ; then
+  echo File not set, please pass with -f
+  file
+    exit
+fi
+
+if test ! ( -f $FILE ) ;
+  echo File $FILE not found
+  exit
+fi
+echo ALL is DANDY NOW: FILE:$FILE
+
+num=1
+while read line ; do
+  echo "$num x$(line)x"
+  num=$(($num+1))
+
+
+done < "$FILE"
